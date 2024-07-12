64a5a73886aef40b6dad531a5f9b61c385403002
AjtanaDjerlek <ajtanadjerlek123@gmail.com>
2024-07-12 19:04:49 +0200

    addedFav

--- src/Components/Favorite/Favorite.jsx
+++ src/Components/Favorite/Favorite.jsx
@@ -1,1 +1,14 @@
-export function Favorite() {}
+// src/Components/Favorite/Favorite.jsx
+import React from 'react';
+import { CenteredBox, StyledImage, MessageText, GoToCoinsLink} from './FavoriteStyled'; // Relativna putanja do stilizovanih komponenti
+import MyFavImage from './No-data-pana.png'; // Relativna putanja do slike
+
+export function Favorite() {
+    return (
+        <CenteredBox>
+            <StyledImage src={MyFavImage} alt="No data" />
+            <MessageText>You haven't added any coin to your favourite list, please add some.</MessageText>
+            <GoToCoinsLink>Go to coins</GoToCoinsLink>
+        </CenteredBox>
+    );
+}
--- src/Components/Favorite/FavoriteStyled.js
+++ src/Components/Favorite/FavoriteStyled.js
@@ -1,1 +1,33 @@
-import { styled } from "@mui/material"
+// src/Components/Favorite/FavoriteStyled.js
+import { Box, styled } from '@mui/material';
+
+// Stiyled Box for center
+export const CenteredBox = styled(Box)({
+    display: 'block',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '400px',
+    textAlign: 'center',
+});
+
+// Styled img element
+export const StyledImage = styled('img')({
+    maxWidth: '100%',
+    height: '400px',
+});
+//Styled Message under Photo
+export const MessageText = styled('p')({
+    marginTop: '5px',
+    fontFamily: 'Arial',
+    fontSize: '15px'
+});
+////Styled CoinsLink text under MessageText
+export const GoToCoinsLink = styled('p')({
+    marginTop: '35px',
+    cursor: 'pointer',
+    fontWeight: 'bold',
+    fontFamily: 'Arial',
+    fontSize: '20px',
+
+
+});
