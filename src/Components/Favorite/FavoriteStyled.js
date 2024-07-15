f694b0cdf632fc82b694c3df0a487a7a10d5b834
AjtanaDjerlek <ajtanadjerlek123@gmail.com>
2024-07-15 17:22:58 +0200

    dataOnFav

--- src/Components/Favorite/FavoriteStyled.js
+++ src/Components/Favorite/FavoriteStyled.js
@@ -1,7 +1,26 @@
-// src/Components/Favorite/FavoriteStyled.js
-import { Box, styled } from "@mui/material"
-import { Link } from "react-router-dom"
 
+import { Link } from "react-router-dom"
+import  { styled, Box } from "@mui/material"
+
+import {TableCell, TableRow}from "@mui/material"
+
+
+
+  export const StyledTableCell = styled(TableCell)({
+    color: "black",
+    fontWeight: "bolder",
+    fontStyle: "normal",
+    fontSize: "0.9em",
+    justifyContent: "space-between",
+    padding: '10px',
+    marginLeft: '20px',
+  })
+  
+  // Black Table Cells for Home Page
+  export const TableCellBlack = styled(TableCell)({
+    color: "black",
+  })
+  
 // Stiyled Box for center
 export const CenteredBox = styled(Box)({
   display: "flex",
