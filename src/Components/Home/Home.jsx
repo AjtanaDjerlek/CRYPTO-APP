f913ee2ff53d0373b745774d67eb40f422bb3901
AjtanaDjerlek <ajtanadjerlek123@gmail.com>
2024-07-16 03:09:16 +0200

    added funcionalty and styling for home/heart button

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,5 +1,4 @@
-import React, { useContext } from "react"
-import { useState } from "react"
+import React, { useContext, useState } from "react"
 import { MyDataContext } from "../../Context/Provider"
 import {
   Table,
@@ -7,13 +6,15 @@ import {
   TableHead,
   TableRow,
   Paper,
-  Typography,
   CircularProgress,
   Box,
   InputAdornment,
   Button,
   IconButton,
+  TableCell,
+  Typography,
 } from "@mui/material"
+import FavoriteRoundedIcon from "@mui/icons-material/FavoriteRounded"
 import { Link } from "react-router-dom"
 import { Line } from "react-chartjs-2"
 import SearchIcon from "@mui/icons-material/Search"
@@ -24,9 +25,7 @@ import {
   HeadingText,
   Top10,
   List,
-  InputPropsStyle,
   StyledTextField,
-  textFieldSx,
   StyledTableContainer,
   StyledTableCell,
   TableCellBlack,
@@ -54,6 +53,7 @@ export function Home({ setFavoriteItemsProp }) {
   const [total, setTotal] = useState("")
   const [selectedCoin, setSelectedCoin] = useState(null)
   const [favoriteItems, setFavoriteItems] = useState([])
+  const [heartButton, setHeartButton] = useState(false) // Dodato stanje heartButton
 
   const addCoinToFavorite = (item) => {
     setFavoriteItems((prevItems) => [...prevItems, item])
@@ -78,7 +78,7 @@ export function Home({ setFavoriteItemsProp }) {
 
   const handleCloseDialog = () => {
     setOpen(false)
-    setAmount("")
+    setAmount(0)
     setTotal("")
     setSelectedCoin(null)
   }
@@ -110,6 +110,21 @@ export function Home({ setFavoriteItemsProp }) {
     ],
   })
 
+  const toggleFavorite = (coin) => {
+    if (favoriteItems.includes(coin)) {
+      // Ako je stavka već omiljena, ukloni je
+      const updatedFavorites = favoriteItems.filter((item) => item !== coin)
+      setFavoriteItems(updatedFavorites)
+      setFavoriteItemsProp(updatedFavorites)
+      setHeartButton(false) // Postavi heartButton na false kad se ukloni
+    } else {
+      // Inače, dodaj stavku u omiljene
+      setFavoriteItems((prevItems) => [...prevItems, coin])
+      setFavoriteItemsProp((prevItems) => [...prevItems, coin])
+      setHeartButton(true) // Postavi heartButton na true kad se doda
+    }
+  }
+
   return (
     <>
       <ImgDiv>
@@ -134,11 +149,9 @@ export function Home({ setFavoriteItemsProp }) {
                   </Box>
                 </InputAdornment>
               ),
-              style: InputPropsStyle,
             }}
-            inputProps={{ style: InputPropsStyle }}
+            value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
-            sx={textFieldSx}
           />
         </Box>
         <StyledTableContainer component={Paper}>
@@ -197,21 +210,26 @@ export function Home({ setFavoriteItemsProp }) {
                       />
                     </div>
                   </TableCellBlack>
-                  <TableCellBlack onClick={() => addCoinToFavorite(coin)}>
-                    <HeartIcon
-                      aria-hidden="true"
-                      focusable="false"
-                      data-prefix="fas"
-                      data-icon="heart"
-                      role="img"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 512 512"
-                    >
-                      <path
-                        fill="currentColor"
-                        d="M458.4 64.3C400.6 15.7 311.3 23 256 79.3 200.7 23 111.4 15.6 53.6 64.3-21.6 127.6-10.6 230.8 43 285.5l175.4 178.7c10 10.2 23.4 15.9 37.6 15.9 14.3 0 27.6-5.6 37.6-15.8L469 285.6c53.5-54.7 64.7-157.9-10.6-221.3zm-23.6 187.5L259.4 430.5c-2.4 2.4-4.4 2.4-6.8 0L77.2 251.8c-36.5-37.2-43.9-107.6 7.3-150.7 38.9-32.7 98.9-27.8 136.5 10.5l35 35.7 35-35.7c37.8-38.5 97.8-43.2 136.5-10.6 51.1 43.1 43.5 113.9 7.3 150.8z"
-                      ></path>
-                    </HeartIcon>
+                  <TableCellBlack onClick={() => toggleFavorite(coin)}>
+                    {!favoriteItems.includes(coin) ? (
+                      <HeartIcon
+                        aria-hidden="true"
+                        focusable="false"
+                        data-prefix="fas"
+                        data-icon="heart"
+                        role="img"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 512 512"
+                        style={{ color: "red" }}
+                      >
+                        <path
+                          fill="#ff0000"
+                          d="M458.4 64.3C400.6 15.7 311.3 23 256 79.3 200.7 23 111.4 15.6 53.6 64.3-21.6 127.6-10.6 230.8 43 285.5l175.4 178.7c10 10.2 23.4 15.9 37.6 15.9 14.3 0 27.6-5.6 37.6-15.8L469 285.6c53.5-54.7 64.7-157.9-10.6-221.3zm-23.6 187.5L259.4 430.5c-2.4 2.4-4.4 2.4-6.8 0L77.2 251.8c-36.5-37.2-43.9-107.6 7.3-150.7 38.9-32.7 98.9-27.8 136.5 10.5l35 35.7 35-35.7c37.8-38.5 97.8-43.2 136.5-10.6 51.1 43.1 43.5 113.9 7.3 150.8z"
+                        ></path>
+                      </HeartIcon>
+                    ) : (
+                      <FavoriteRoundedIcon style={{ color: "red" }} />
+                    )}
                   </TableCellBlack>
                   <TableCellBlack>
                     <IconButton
